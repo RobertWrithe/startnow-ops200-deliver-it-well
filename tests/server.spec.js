31f2f984d0b5b04f18888fbcb938db6bd35ce36c
Robert Barnett <therobertbarnett@gmail.com>
2018-09-26 13:53:05 -0700

    Ready for deployment

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page tells a dumb joke', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Knock knock');
+        done();
+      });
+    });
 })
